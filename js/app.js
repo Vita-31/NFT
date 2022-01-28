4c1c64b1db6bca9ab6c4a3c3f8e01a50a18a05af
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-01-28 21:59:22 +0200

    add partners section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const tabBlocks = document.getElementById('tabs');
+
 if(tabBlocks) {
     const tabSection = tabBlocks.querySelectorAll('.art__cards');
 
@@ -7,11 +8,11 @@ if(tabBlocks) {
         const tabItem = e.target.closest('.tab__item');
         const tabItemData = tabItem.dataset.id
         const tabItemActive = document.querySelectorAll('.tab__item_active');
-        console.log(tabItemActive)
         tabItemActive.forEach(itemActive => {
             itemActive.classList.remove('tab__item_active')
         })
         tabItem.classList.add('tab__item_active')
+        
         tabSection.forEach(section => {
             const sectionData = section.dataset.section;
             if(tabItemData === sectionData) {
