7cf907ff45f34a34860e162291efeaddcca5ff75
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-01-28 21:48:21 +0200

    toggle tabs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 const tabBlocks = document.getElementById('tabs');
 if(tabBlocks) {
+    const tabSection = tabBlocks.querySelectorAll('.art__cards');
+
     tabBlocks.addEventListener('click', e => {
+        e.preventDefault();
         const tabItem = e.target.closest('.tab__item');
         const tabItemData = tabItem.dataset.id
+        const tabItemActive = document.querySelectorAll('.tab__item_active');
+        console.log(tabItemActive)
+        tabItemActive.forEach(itemActive => {
+            itemActive.classList.remove('tab__item_active')
+        })
+        tabItem.classList.add('tab__item_active')
+        tabSection.forEach(section => {
+            const sectionData = section.dataset.section;
+            if(tabItemData === sectionData) {
+                section.classList.add('art__cards_visible');
+            } else {
+                section.classList.remove('art__cards_visible')
+            }
+        })
     })
 }
